03041e53040222f3d1fddb826c45cf3f017b4fbb
agn3y <agneypatel96@gmail.com>
2025-01-17 16:54:38 +0530

    bunch of code

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 console.log("bharat mata ki jay");
 
-<<<<<<< Updated upstream
-let number = 10
-function oneToten() {
-    for(i = 1; i <= number; i++){
-        console.log(i);
-    }
-}
-
-oneToten()
-=======
 function print() {
     let number = 10;
     for (let i = 1; i <= number; i++) {
@@ -17,4 +7,3 @@ function print() {
     }
 }
 print();
->>>>>>> Stashed changes
