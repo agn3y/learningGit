0a0a624a2ee0c5813c49a55686fe2b95ab866631
agn3y <agneypatel96@gmail.com>
2025-01-17 16:47:35 +0530

    trying to solve the error

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log("bharat mata ki jay");
 
+<<<<<<< Updated upstream
 let number = 10
 function oneToten() {
     for(i = 1; i <= number; i++){
@@ -8,3 +9,12 @@ function oneToten() {
 }
 
 oneToten()
+=======
+function print() {
+    let number = 10;
+    for (let i = 1; i <= number; i++) {
+        console.log(i); 
+    }
+}
+print();
+>>>>>>> Stashed changes
